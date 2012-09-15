7703c353672c2e8afa51ab4b11d8c30fe36ac683
tomykaira <tomykaira@gmail.com>
2012-09-15 09:44:09 +0900

    output written value

--- mem.s
+++ mem.s
@@ -1,19 +1,7 @@
 	mvhi	$r3, 0
 	mvlo	$r3, 4
-	mvhi	$r4, 0
-	mvlo	$r4, 8
 	sti	$r3, $r0, 102
-	nop
-	nop
-	nop
-	nop
+	outputb $r3
 	ldi	$r5, $r0, 102
-	nop
-	nop
-	nop
-	nop
-	outputb $r5
-	sti	$r4, $r0, 92
-	ldi	$r5, $r0, 92
 	outputb $r5
 	halt
