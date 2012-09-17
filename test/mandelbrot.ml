acc898063be61fc5d6d25341f6fbe86bc020ad0f
tomykaira <tomykaira@gmail.com>
2012-09-17 19:42:37 +0900

    Rewrite mandelbrot for mincaml

--- test/mandelbrot.ml
+++ test/mandelbrot.ml
@@ -1,18 +1,17 @@
-let test x y =
-  let abs = 2.0 ** 2. in
-  let (c_x, c_y) = (x, y) in
+let rec test x y =
+  let abs = 2. *. 2. in
   let rec escapeq z_x z_y iter =
-    if z_x ** 2. +. z_y ** 2. >= abs then true
-    else if iter == 0 then false
-    else escapeq (c_x +. z_x ** 2. -. z_y ** 2.) (c_y +. 2. *. z_x *. z_y) (iter-1)
+    if z_x *. z_x +. z_y *. z_y >= abs then true
+    else if iter = 0 then false
+    else escapeq (x +. z_x *. z_x -. z_y *. z_y) (y +. 2. *. z_x *. z_y) (iter-1)
   in
   escapeq 0.0 0.0 200
-;;
+in
 
-let mandelbrot start_x start_y step_x step_y end_x end_y =
+let rec mandelbrot start_x start_y step_x step_y end_x end_y =
   let rec loop_y y =
     let rec loop_x x =
-      print_char (if test x y then '0' else '1');
+      print_char (if test x y then 48 else 49);
       if x >= end_x then () else loop_x (x +. step_x)
     in
     loop_x start_x;
@@ -20,9 +19,17 @@ let mandelbrot start_x start_y step_x step_y end_x end_y =
     if y >= end_y then () else loop_y (y +. step_y)
   in
   loop_y start_y
-;;
+in
 
-print_char 'P'; print_char '1'; print_newline () ;;
-print_int 301; print_char ' '; print_int 201; print_newline () ;; (* XxY*)
-print_int 255; print_newline () ;;
-mandelbrot (-.2.0) (-.1.0) 0.01 0.01 (1.0) (1.0)
+let _ = (
+  print_char 80;
+  print_char 49;
+  print_newline ();
+
+  print_int 301;
+  print_char 32;
+  print_int 201;
+  print_newline (); (* XxY*)
+
+  mandelbrot (-2.0) (-1.0) 0.01 0.01 (1.0) (1.0)
+) in 0
