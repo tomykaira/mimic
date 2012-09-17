b7ced176572814403ddd7ded51094e812bc4b101
tomykaira <tomykaira@gmail.com>
2012-09-17 18:56:14 +0900

    Revert "Mandelbrot coloring failure"
    This reverts commit 9b18857a96f03235a2f5f718a35de6c083e72b6e.

--- test/mandelbrot.ml
+++ test/mandelbrot.ml
@@ -1,9 +1,9 @@
 let test x y =
+  let abs = 2.0 ** 2. in
   let (c_x, c_y) = (x, y) in
   let rec escapeq z_x z_y iter =
-    let norm = sqrt (z_x ** 2. +. z_y ** 2.) in
-    if norm >= 2. then 2.
-    else if iter == 0 then norm
+    if z_x ** 2. +. z_y ** 2. >= abs then true
+    else if iter == 0 then false
     else escapeq (c_x +. z_x ** 2. -. z_y ** 2.) (c_y +. 2. *. z_x *. z_y) (iter-1)
   in
   escapeq 0.0 0.0 200
@@ -12,8 +12,7 @@ let test x y =
 let mandelbrot start_x start_y step_x step_y end_x end_y =
   let rec loop_y y =
     let rec loop_x x =
-      print_int (int_of_float (test x y *. 100.));
-      print_char ' ';
+      print_char (if test x y then '0' else '1');
       if x >= end_x then () else loop_x (x +. step_x)
     in
     loop_x start_x;
@@ -23,7 +22,7 @@ let mandelbrot start_x start_y step_x step_y end_x end_y =
   loop_y start_y
 ;;
 
-print_char 'P'; print_char '2'; print_newline () ;;
+print_char 'P'; print_char '1'; print_newline () ;;
 print_int 301; print_char ' '; print_int 201; print_newline () ;; (* XxY*)
-print_int 200; print_newline () ;;
+print_int 255; print_newline () ;;
 mandelbrot (-.2.0) (-.1.0) 0.01 0.01 (1.0) (1.0)
