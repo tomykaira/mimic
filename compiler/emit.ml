d1534128fb2ca2a33be1d554d4d8f504e66bcf19
tomykaira <tomykaira@gmail.com>
2012-09-17 23:04:04 +0900

    Revert "Use small memory to test on simulator" to use full memory for mandelbrot
    This reverts commit 522c5edeec23b7a76242c2082ffec7fdcf7b4087.

--- compiler/emit.ml
+++ compiler/emit.ml
@@ -505,8 +505,8 @@ let f oc (Prog(fundefs, e)) =
 	Output.add_stmt (Output.Sub (reg_m1, reg_0, reg_p1));
 	(* EDITED: frame pointer をうえのほうに置いてくる *)
 	(* 0x1fffff *)
-	Output.add_stmt (Output.Mvhi (reg_sp, 0x0));
-	Output.add_stmt (Output.Mvlo (reg_sp, 1023));
+	Output.add_stmt (Output.Mvhi (reg_sp, 0x1f));
+	Output.add_stmt (Output.Mvlo (reg_sp, 0xffff));
 	g oc (NonTail reg_0, e);
 	Output.add_stmt Output.Halt;
 
