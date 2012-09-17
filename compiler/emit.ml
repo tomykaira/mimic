cc4d27be0da7c86b653e9b5c235cd97fe3efc2ee
tomykaira <tomykaira@gmail.com>
2012-09-17 12:55:38 +0900

    back to the default

--- compiler/emit.ml
+++ compiler/emit.ml
@@ -503,9 +503,6 @@ let f oc (Prog(fundefs, e)) =
 	Output.add_stmt (Output.Mvlo (reg_hp, pos mod (1 lsl 16)));
 	Output.add_stmt (Output.Addi (reg_p1, reg_0, 1));
 	Output.add_stmt (Output.Sub (reg_m1, reg_0, reg_p1));
-	(* EDITED: frame pointer をうえのほうに置いてくる *)
-	Output.add_stmt (Output.Mvhi (reg_sp, 1));
-	Output.add_stmt (Output.Mvlo (reg_sp, 0)); 
 	g oc (NonTail reg_0, e);
 	Output.add_stmt Output.Halt;
 
--- simulator/simulator.cpp
+++ simulator/simulator.cpp
@@ -218,8 +218,7 @@ int simulate(char* srcPath)
 	conv tmp1;
 
 	// 初期化
-	// FR = sizeof(RAM) / 4 - 1;
-	FR = 0;
+	FR = sizeof(RAM) / 4 - 1;
 	// cerr << "FR = " << FR << endl;
 
 	int internal_stack[CALL_STACK_SIZE];
