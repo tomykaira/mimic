4efe4e071f3ef7bf389476c0fcc3dae3aca5f1d1
tomykaira <tomykaira@gmail.com>
2012-10-04 17:47:38 +0900

    extern for C from CPP

--- simulator/fpu.h
+++ simulator/fpu.h
@@ -7,8 +7,11 @@
 
 typedef union{	uint32_t i; float f;} conv;
 
-void load_tables();
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+void load_tables();
 uint32_t myfadd(uint32_t rs, uint32_t rt);
 uint32_t myfsub(uint32_t rs, uint32_t rt);
 uint32_t myfmul(uint32_t rs, uint32_t rt);
@@ -22,5 +25,8 @@ uint32_t myfsin(uint32_t rs);
 uint32_t myfcos(uint32_t rs);
 uint32_t myftan(uint32_t rs);
 uint32_t myfatan(uint32_t rs);
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* _FPU_H_ */
